9e6a8ea72f558591a7c56d63253c3d6dc9ff1cd7
pranavkhapra <pranavkhapra23@gmail.com>
2021-04-15 21:27:25 +0530

    basically all the routing and all of the pages

--- tailwind-css-v1/src/App.js
+++ tailwind-css-v1/src/App.js
@@ -1,14 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Content from './components/Content';
-import Hero from './components/Hero';
+import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import Home from './pages';
+import about from './pages/about';
+import contact from './pages/contact';
+import menu from './pages/menu';
 
 function App() {
   return (
     <>
       <NavBar />
-      <Hero />
-      <Content />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={about} />
+        <Route path="/menu" exact component={menu} />
+        <Route path="/contact" exact component={contact} />
+      </Switch>
+      <Footer />
     </>
   );
 }
---
+++ tailwind-css-v1/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Content from '../components/Content';
+import Hero from '../components/Hero';
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <Content />
+    </div>
+  );
+}
+
+export default Home;
