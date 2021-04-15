3c38b9f3393da597a744f8cfac3e948ba0ad8a39
pranavkhapra <pranavkhapra23@gmail.com>
2021-04-15 17:21:47 +0530

    added the content component type and all

--- tailwind-css-v1/src/components/Content.js
+++ tailwind-css-v1/src/components/Content.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import imageOne from '../images/egg.jpg';
+import imageTwo from '../images/egg1.jpg';
 
 function Content() {
-  return <div />;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center bg-white font-mono h-screen py-40">
+        <img
+          src={imageOne}
+          alt="egg1"
+          className="h-full rounded shadow mb-16"
+        />
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-2xl mb-2">Egg Muffins</h2>
+          <p className="mb-2">Crispy,delicious and nutritious</p>
+          <span>$16</span>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center bg-white font-mono h-screen py-40">
+        <img
+          src={imageTwo}
+          alt="egg2"
+          className="h-full rounded shadow mb-16"
+        />
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-2xl mb-2">Egg Salad</h2>
+          <p className="mb-2">Crispy,delicious and nutritious</p>
+          <span>$18</span>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Content;
