c9a5142256caaad35808c4eae6eeb98964f34627
pranavkhapra <pranavkhapra23@gmail.com>
2021-04-17 18:08:16 +0530

    huuu iam tired unproductive

--- react-v2/src/components/Hero.js
+++ react-v2/src/components/Hero.js
@@ -1,29 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import planetOne from '../images/planet.svg';
+import planetTwo from '../images/planet-2.svg';
+import planetThird from '../images/planet-3.svg';
+import planetFour from '../images/planet-4.svg';
 
-const Section = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #131313;
-`;
-const Container = styled.div``;
-const ColumnLeft = styled.div``;
-const ColumnRight = styled.div``;
-const Button = styled.div``;
 function Hero() {
   return (
-    <Section>
-      <Container>
-        <ColumnLeft>
-          <h1>Welcome to Space</h1>
-          <p>Journey to the Unknown</p>
-          <Button>Get Started</Button>
-        </ColumnLeft>
-        <ColumnRight>filler content</ColumnRight>
-      </Container>
-    </Section>
+    <div className="bg-black h-screen flex justify-between">
+      <div className="h-screen text-white flex flex-col justify-center items-start align-left ">
+        <h1 className="sm:text-2xl md:text-4xl lg:text-16xl ml-32 mb-4">
+          Welcome to Space
+        </h1>
+        <p className="sm:text-2xl md:text-4xl lg:text-16xl ml-32">
+          Journey to the Unknown
+        </p>
+        <button
+          className="bg-purple-500 py-4 px-4 mt-12 ml-48 rounded-lg"
+          type="button"
+        >
+          Get Started
+        </button>
+      </div>
+      <div className="h-screen flex flex-col justify-center items-center align-right">
+        <img src={planetOne} alt="planet1" />
+        <img src={planetTwo} alt="planet2" />
+        <img src={planetThird} alt="planet3" />
+        {/* <img src={planetFour} alt="planet4" /> */}
+      </div>
+    </div>
   );
 }
 
